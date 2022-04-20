acc71c1a92c8e0bf12262166c73fd2dc0262aab8
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-21 00:54:08 +0100

    install mui dependencies and move React to v17

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
 // import './index.css';
 const Home = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <Button
+      startIcon={<SaveIcon />}
+      // endIcon={<SaveIcon />}
+      size="small"
+      href="#"
+      variant="contained"
+      color="primary"
+      // m: margin and p: padding
+      // sx={{ m: 2, p: 2 }}
+      // disabled
+    >
+      SAVE
+    </Button>
+  );
 };
 
 export default Home;
