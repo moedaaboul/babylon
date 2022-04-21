a36233e18d7f4fe42421eba640c2d2b650b48b7d
hy <yang.hrn@gmail.com>
2022-04-21 22:34:35 +0100

    merging cart to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import {
   Favorite as FavouriteIcon,
   Person as PersonIcon,
   ShoppingBag as ShoppingBagIcon,
-} from '@mui/icons-material';
+} from "@mui/icons-material";
 
 import {
   AppBar,
@@ -17,31 +17,33 @@ import {
   Tooltip,
   Typography,
   IconButton,
-} from '@mui/material';
-import React from 'react';
+} from "@mui/material";
+
+import SideCart from "../SideCart";
+import React from "react";
 
 const StyledToolbar = styled(Toolbar)({
-  display: 'flex',
-  justifyContent: 'space-between',
+  display: "flex",
+  justifyContent: "space-between",
 });
 
 const StyledIconButton = styled(IconButton)({
-  color: 'inherit',
+  color: "inherit",
 });
 
-const Search = styled('div')(({ theme }) => ({
-  backgroundColor: 'white',
-  padding: '0 10px',
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
-  width: '40%',
+  width: "40%",
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-  display: 'none',
-  alignItems: 'center',
-  gap: '20px',
-  [theme.breakpoints.up('sm')]: {
-    display: 'flex',
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
   },
 }));
 
@@ -61,13 +63,13 @@ const Navbar = () => {
         <StyledToolbar>
           <Typography
             variant="h6"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+            sx={{ display: { xs: "none", sm: "block" } }}
           >
             BABYLON
           </Typography>
-          <AutoAwesomeIcon sx={{ display: { xs: 'block', sm: 'none' } }} />
+          <AutoAwesomeIcon sx={{ display: { xs: "block", sm: "none" } }} />
           <Search>
-            <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
               <SearchIcon />
             </IconButton>
             <InputBase placeholder="search..." />
@@ -85,7 +87,7 @@ const Navbar = () => {
             </Tooltip>
             <Tooltip title="Shopping Bag">
               <StyledIconButton>
-                <ShoppingBagIcon />
+                <SideCart location="Right" />
               </StyledIconButton>
             </Tooltip>
           </Icons>
@@ -99,31 +101,31 @@ const Navbar = () => {
           PaperProps={{
             elevation: 0,
             sx: {
-              overflow: 'visible',
-              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
               mt: 1.5,
-              '& .MuiAvatar-root': {
+              "& .MuiAvatar-root": {
                 width: 32,
                 height: 32,
                 ml: -0.5,
                 mr: 1,
               },
-              '&:before': {
+              "&:before": {
                 content: '""',
-                display: 'block',
-                position: 'absolute',
+                display: "block",
+                position: "absolute",
                 top: 0,
                 right: 14,
                 width: 10,
                 height: 10,
-                bgcolor: 'background.paper',
-                transform: 'translateY(-50%) rotate(45deg)',
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
                 zIndex: 0,
               },
             },
           }}
-          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
         >
           <MenuItem>Login</MenuItem>
           {/* <MenuItem>Profile</MenuItem> */}
