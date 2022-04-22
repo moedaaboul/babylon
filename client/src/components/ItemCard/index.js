f216ee47ec58d1945f73c8de39b4c6c95925a398
Vienna Borowska <vienna.borowska@hotmail.com>
2022-04-22 22:21:19 +0100

    Styled item cards into grid using MUI

--- client/src/components/ItemCard/index.js
+++ client/src/components/ItemCard/index.js
@@ -1,40 +1,53 @@
 import * as React from "react";
+import { shadows } from "@mui/system";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+// import { width } from "@mui/system";
+// import "./ItemCard.css";
 
 export default function itemCard({ items }) {
   return (
-    <div className="card-wrapper">
-      {items &&
-        items.map((item) => (
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                src={process.env.PUBLIC_URL + item.image}
-                alt="img placeholder"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {item.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {item.description}
-                </Typography>
-                <Typography variant="body3" color="text.secondary">
-                  £{item.price}
-                </Typography>
-                <Typography variant="body3" color="text.secondary">
-                  {item.size}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {items &&
+          items.map((item) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+              <Card height="600" sx={{ maxWidth: 345, boxShadow: 2 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="400"
+                    src={process.env.PUBLIC_URL + item.image}
+                    alt="img placeholder"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      gutterBottom
+                      variant="body2"
+                      color="text.secondary"
+                    >
+                      {item.description}
+                    </Typography>
+                    <Box sx={{ textAlign: "center", fontWeight: "bold" }}>
+                      <Typography variant="body3" color="text.secondary">
+                        £{item.price}
+                      </Typography>
+                    </Box>
+                    <br></br>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+      </Grid>
+    </Box>
   );
 }
--- client/src/pages/AllItems/index.js
+++ client/src/pages/AllItems/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import ItemCard from "../../components/ItemCard";
 import { QUERY_ITEMS } from "../../utils/queries";
-// import { Grid } from
 
 const AllItems = () => {
   const { loading, error, data } = useQuery(QUERY_ITEMS);
@@ -10,17 +9,17 @@ const AllItems = () => {
   console.log(data);
   const items = data?.items || [];
   console.log(items);
-  // const destruct = { ...data };
-  // console.log("DESTRUCTURED", destruct);
-  // const items = { ...data };
-  // console.log(items);
-  // const items = data?.items || [];
+
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-  // console.log(items);
+
   return (
     <div className="all-items-container">
-      <ItemCard items={items} title="Here's the current roster of friends..." />
+      <ItemCard
+        items={items}
+        key={items._id}
+        title="Here's the current available items..."
+      />
     </div>
   );
 };
