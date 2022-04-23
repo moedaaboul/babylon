84a502fd48ac8d1642b28aaae210cd8ad31d8e4c
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-23 19:55:06 +0100

    enable preview of images on Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,28 @@
-import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import SingleProduct from "./pages/SingleProduct";
-import Register from "./pages/Register";
-import AllItems from "./pages/AllItems";
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SingleProduct from './pages/SingleProduct';
+import Register from './pages/Register';
+import AllItems from './pages/AllItems';
+import Dashboard from './pages/Dashboard';
 
-import ErrorBoundary from "./components/ErrorBoundary";
+import ErrorBoundary from './components/ErrorBoundary';
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
+  const token = localStorage.getItem('id_token');
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
@@ -48,6 +44,7 @@ function App() {
             <Route path="/items" element={<AllItems />} />
             <Route path="/SingleProduct" element={<SingleProduct />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="*" element={<h1 className="">Wrong page!</h1>} />
           </Routes>
         </ErrorBoundary>
