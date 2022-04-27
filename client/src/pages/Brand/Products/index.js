97fd0034f424a193568a68b61a9b99d418a2ba84
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-27 23:52:58 +0100

    rendered brandItems in dashboard

--- client/src/pages/Brand/Products/index.js
+++ client/src/pages/Brand/Products/index.js
@@ -1,56 +1,53 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { useQuery } from '@apollo/client';
+import { GET_BRAND_ITEMS } from '../../../utils/queries';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: '_id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'title',
+    headerName: 'Name',
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
+    field: 'stock',
+    headerName: 'In Stock',
     type: 'number',
-    width: 110,
+    width: 70,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'price',
+    headerName: 'Sale Price',
+    type: 'number',
+    width: 90,
+    editable: true,
   },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    field: 'description',
+    headerName: 'Description',
+    width: 250,
+    editable: true,
+  },
 ];
 
 export default function DataGridDemo() {
+  const { loading, data } = useQuery(GET_BRAND_ITEMS);
+  const productData = data?.brandItems || {};
+  console.log(productData);
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+
   return (
     <DataGrid
-      sx={{ height: 400, width: 730 }}
-      rows={rows}
+      sx={{ height: 430, width: 730 }}
+      rows={productData}
       columns={columns}
       pageSize={5}
+      getRowId={(row) => row._id}
       rowsPerPageOptions={[5]}
       checkboxSelection
       disableSelectionOnClick
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,17 +14,14 @@ export const QUERY_ITEMS = gql`
   }
 `;
 
-export const QUERY_BRAND_ITEMS = gql`
+export const GET_BRAND_ITEMS = gql`
   query Query {
     brandItems {
       _id
       title
       description
-      image
       price
       stock
-      size
-      brand
     }
   }
 `;
