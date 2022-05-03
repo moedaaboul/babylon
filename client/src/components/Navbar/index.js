99fb2bafb1c2464a5bc1982436ae894c82ce8873
hy <yang.hrn@gmail.com>
2022-05-03 23:54:56 +0100

    aigning checkout

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -85,7 +85,7 @@ const Navbar = () => {
     <>
       <AppBar position="sticky">
         <StyledToolbar>
-          <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }} href="/">
             BABYLON
           </Typography>
           <AutoAwesomeIcon sx={{ display: { xs: 'block', sm: 'none' } }} />
