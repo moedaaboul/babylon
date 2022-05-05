4a8994b229c85ece9b407b1edf8e5b2ce7b5bf41
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-05-05 12:15:21 +0100

    styled remove from wish list button

--- client/src/pages/WishList/WishCard.js
+++ client/src/pages/WishList/WishCard.js
@@ -7,12 +7,14 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import IconButton from '@mui/material/IconButton';
 import Label from '../../components/Label';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import ClearIcon from '@mui/icons-material/Clear';
 import { useMutation } from '@apollo/client';
 import { TOGGLE_LIKE } from '../../utils/mutations';
 import { QUERY_WISH_LIST } from '../../utils/queries';
@@ -71,40 +73,17 @@ export default function WishCard({ item }) {
 
   return (
     <Card sx={{ borderRadius: '16px' }}>
-      <CardActionArea component={RouterLink} to={`/item/${_id}`}>
+      <CardActionArea>
         <Box sx={{ pt: '100%', position: 'relative' }}>
-          {discountedPrice && (
-            <Label
-              variant="filled"
-              sx={{
-                color: '#FFFFFF',
-                bgcolor: '#FF0000',
-                zIndex: 9,
-                top: 16,
-                right: 16,
-                position: 'absolute',
-                textTransform: 'uppercase',
-              }}>
-              Sale
-            </Label>
-          )}
-          {featured && (
-            <Label
-              variant="filled"
-              sx={{
-                color: '#FFFFFF',
-                bgcolor: '#FFAC1C',
-                zIndex: 9,
-                top: 16,
-                right: 16,
-                position: 'absolute',
-                textTransform: 'uppercase',
-              }}>
-              Featured
-            </Label>
-          )}
           {/* onMouseOver={(this.src = handleImageChange())} */}
-          <ProductImgStyle alt={title} src={image[0]} />
+          <Link to={`/item/${_id}`} component={RouterLink}>
+            <ProductImgStyle alt={title} src={image[0]} />
+          </Link>
+          <Tooltip title="Remove from Wish list" style={{ position: 'absolute', top: 0, right: 3, color: 'white' }}>
+            <IconButton aria-label="remove from wishlist" onClick={() => handleToggleLike(_id)}>
+              <ClearIcon />
+            </IconButton>
+          </Tooltip>
         </Box>
       </CardActionArea>
       <Stack spacing={2} sx={{ p: 3 }}>
@@ -129,10 +108,9 @@ export default function WishCard({ item }) {
               {discountedPrice && fCurrency(price)}
             </Typography>
           </Typography>
-
-          <Tooltip title="Remove from Wish list">
-            <IconButton aria-label="remove from wishlist" onClick={() => handleToggleLike(_id)}>
-              {true ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          <Tooltip title="Add to Bag">
+            <IconButton aria-label="add to bag" onClick={() => handleToggleLike(_id)}>
+              <ShoppingBagIcon />
             </IconButton>
           </Tooltip>
         </Stack>
