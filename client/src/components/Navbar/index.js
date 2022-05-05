9f4a1b7d427fd22860a163bea2f263d5a8c18b74
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-05-05 09:58:10 +0100

    replaced navbar header name with logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -98,7 +98,7 @@ const Navbar = () => {
       <AppBar position="sticky">
         <StyledToolbar>
           <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }} onClick={redirectToHome}>
-            BABYLON
+            🅱🅰🅱🆈🅻🅾🅽
           </Typography>
           <AutoAwesomeIcon sx={{ display: { xs: 'block', sm: 'none' } }} />
           <Search>
