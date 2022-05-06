e4752d83c4410952d7dca8a3d58cd35982d9266b
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-05-06 18:47:44 +0100

    add wildcard route

--- server/server.js
+++ server/server.js
@@ -34,6 +34,10 @@ app.get('/success', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
