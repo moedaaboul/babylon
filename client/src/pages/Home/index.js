283e3fc6539e6a17858d858e949e256eaf28bb48
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-05-09 17:13:50 +0100

    home page improvements

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -12,6 +12,8 @@ import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 // import SideCart from "../../components/SideCart";
 // import './index.css';
 const Home = () => {
@@ -48,6 +50,82 @@ const Home = () => {
                 width="500px"
                 height="auto"
                 style={{ aspectRatio: 16 / 9 }}></img>
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box style={{ background: 'white', margin: 50 }}></Box>
+      <Box style={{ background: 'rgb(250, 234, 173)', width: '100vw' }}>
+        <Grid
+          container
+          rowSpacing={1}
+          style={{ paddingTop: '50px', paddingLeft: '20px' }}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={12} sm={6}>
+            <Stack alignItems="flex-start" justifyContent="center" direction="column" spacing={1}>
+              <Typography style={{ fontWeight: 'bolder' }} variant="h5">
+                Fashion with a cause
+              </Typography>
+              <Typography variant="h5">Support reduced emissions</Typography>
+              <Link component={RouterLink} to="/items" variant="body2" style={{ textDecoration: 'none' }}>
+                <Button
+                  variant="text"
+                  disableRipple
+                  style={{ textTransfrom: 'inherit', color: 'black', fontWeight: 'bolder' }}>
+                  Shop now <ArrowForwardIcon style={{ marginLeft: '5px' }} />
+                </Button>
+              </Link>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={5}>
+            <Container>
+              <Carousel />
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box style={{ background: 'white', margin: 50 }}></Box>
+      <Box style={{ background: 'rgb(70, 154, 200)', width: '100vw' }}>
+        <Grid
+          container
+          rowSpacing={1}
+          style={{ paddingTop: '50px', paddingLeft: '20px' }}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={12} sm={6}>
+            <Stack alignItems="flex-start" justifyContent="center" direction="column" spacing={1}>
+              <Typography style={{ fontWeight: 'bolder' }} variant="h5">
+                Joyful experience
+              </Typography>
+              <Typography variant="h5">More precious moments</Typography>
+              <Link component={RouterLink} to="/items" variant="body2" style={{ textDecoration: 'none' }}>
+                <Button
+                  variant="text"
+                  disableRipple
+                  style={{ textTransfrom: 'inherit', color: 'black', fontWeight: 'bolder' }}>
+                  Discover now <ArrowForwardIcon style={{ marginLeft: '5px' }} />
+                </Button>
+              </Link>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={5}>
+            <Container>
+              <img
+                width="50%"
+                height="auto"
+                src={`https://images.unsplash.com/photo-1531983412531-1f49a365ffed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`}
+                srcSet={`https://images.unsplash.com/photo-1531983412531-1f49a365ffed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80 2x`}
+                alt="hats"
+                loading="lazy"
+              />
+
+              <img
+                width="50%"
+                height="auto"
+                src={`https://images.unsplash.com/flagged/photo-1573496760140-f15e236b5801?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`}
+                srcSet={`https://images.unsplash.com/flagged/photo-1573496760140-f15e236b5801?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80 2x`}
+                alt="hats"
+                loading="lazy"
+              />
             </Container>
           </Grid>
         </Grid>
